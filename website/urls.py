1c45a01dbfdb47d943b56eb10ba7361f88d72071
BhanuHarshitha <bhanuharshitha02@gmail.com>
2023-01-19 22:08:07 +0530

    added about.html and routing

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/',views.home,name='website-home'),
+    path('', views.home, name='website-home'),
+    path('about/', views.about, name='website-about'),
 ]
