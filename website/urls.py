53a566e3eb4d89bbd0fc36e51293c0f1199a8a4d
BhanuHarshitha <bhanuharshitha02@gmail.com>
2023-01-21 20:45:49 +0530

    designed home page

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='website-home'),
-    path('about/', views.about, name='website-about'),
+    path('', views.home,name='website-home'),
+    #path("simple_function",view.simple_function)
+    path('about/',views.about,name='website-about'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    #temp=loader.get_template('htmlfiles\main.html')
-    return render(request, 'website/home.html')
+    return render(request,'website/home.html')
 
 def about(request):
-    return render(request, 'website/about.html')
+    return render(request,'website/about.html')
